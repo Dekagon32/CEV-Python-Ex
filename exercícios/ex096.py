6466fa034b91956860bbc1beef45eb8ff7172184
Dekagon <devilynmarra@gmail.com>
2020-06-30 22:25:55 -0300

    exercícios concluídos
    Exercícios 96 concluídoe 97 começado.

---
+++ exercícios/ex096.py
@@ -0,0 +1,4 @@
+def área(x,y):
+    print(f'A área de {x}x{y} é {x*y}')
+
+área(int(input('Digite X: ')),int(input('Digite Y: ')))
---
+++ exercícios/ex097.py
@@ -0,0 +1,6 @@
+def escreva(arg):
+    print('~'*(len(arg)+2))
+    print('{:^}'.format(arg))
+    print('~'*(len(arg)+2))
+
+escreva('top')
