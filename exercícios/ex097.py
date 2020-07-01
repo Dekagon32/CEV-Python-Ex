07a38335e8df2e237713ed5cb7a2ca628bd67cf2
Dekagon <devilynmarra@gmail.com>
2020-06-30 23:42:11 -0300

    Términio
    Exercício 97 completo

--- exercícios/ex097.py
+++ exercícios/ex097.py
@@ -1,6 +1,6 @@
 def escreva(arg):
-    print('~'*(len(arg)+2))
-    print('{:^}'.format(arg))
-    print('~'*(len(arg)+2))
+    print('~'*(len(arg)+4))
+    print('  {}'.format(arg))
+    print('~'*(len(arg)+4))
 
-escreva('top')
+escreva('O que qcê qué bro')
