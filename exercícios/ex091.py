9d5af584fa70c2c75e6ed2bcd115d5b3a94e7ad8
Dekagon <devilynmarra@gmail.com>
2020-06-30 23:20:42 -0300

    Reparo
    Exercício 95 reparado e exercício 91 terminado

--- exercícios/ex091.py
+++ exercícios/ex091.py
@@ -1,8 +1,13 @@
 from random import randint
+from operator import itemgetter
 
 players = {}
-
+rank = dict()
+print('Valores sorteados: ')
 for c in range(1,5):
     players['p{}'.format(c)] = randint(1,6)
-    print(players[f'p{c}')
-print(players,mai,men)
+    print('O Player {} tirou {} pontos.'.format(c,players[f'p{c}']))
+rank = sorted(players.items(),key=itemgetter(1), reverse=True)
+print('-'*27)
+for i,v in enumerate(rank):
+    print(f'{i+1}º lugar: Player {v[0][1]} com {v[1]} pts.')
